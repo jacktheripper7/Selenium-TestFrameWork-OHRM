3f61803088f870f6fe5ea50fe673dbe75e9edda4
Debashish Sarkar <jacktheripper.mystikal@gmail.com>
2025-06-27 21:21:40 +0530

    Test added comment

--- src/test/java/com/orangehrm/test/SampleTest.java
+++ src/test/java/com/orangehrm/test/SampleTest.java
@@ -10,7 +10,6 @@ import static org.testng.AssertJUnit.assertEquals;
 public class SampleTest extends BaseClass {
     @Test
     public void sampleTest() {
-        //Adding a comment to test the git
         ExtentManager.logStep("Navigating to OrangeHRM");
         String title = getDriver().getTitle();
         ExtentManager.logStep("Verifying the title of the page");
